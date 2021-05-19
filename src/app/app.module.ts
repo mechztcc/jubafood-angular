5c305c917b9e26d8a8fc3639b523893fb9b2c840
Alberto Paiva <dojathan@gmail.com>
2021-05-19 12:21:55 -0300

    header slider created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/templates/header/header.component';
+import { FooterComponent } from './components/templates/footer/footer.component';
+import { SliderComponent } from './components/templates/slider/slider.component';
+import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    NavComponent,
+
   ],
   imports: [
     BrowserModule,
