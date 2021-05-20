38ec3b3c373a0353e9b0715aac5747c86ea7d316
Alberto Paiva <dojathan@gmail.com>
2021-05-20 18:52:50 -0300

    created app-slider

--- src/app/components/template/template.module.ts
+++ src/app/components/template/template.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    SliderComponent
   ]
 })
 export class TemplateModule { }
